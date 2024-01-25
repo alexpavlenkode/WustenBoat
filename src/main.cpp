0faa63d4b2894346bfa4ca2d671232134e42462d
Alex Pavlenko <alepavlenko@mail.ru>
2024-01-25 17:54:24 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+/*
+  WustenBoat: Autonomous Boat for Wüsteneutzsch See Exploration
+  Main program file for controlling the boat, shoreline detection,
+  depth measurement, and data transmission to a mobile device.
+
+  Автор: Alex
+  Дата: 25/01/2024
+
+*/
